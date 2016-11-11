984fd22e7d3dc0dea55917835ff5bc96465104b4
Knut Andreas Ruud <kar@dips.no>
2016-11-11 14:38:02 +0100

    Added Clip class, some fixes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 ///<reference path="big-def.ts"/>
+///<reference path="Clip.ts"/>
 
+declare function post(msg: string);
+
+/*
 var a: IBig = BigFactory.create(1.5);
 var b: IBig = BigFactory.create(0.7);
 
 console.log(a.toFixed(4));
 console.log(b.lt(a));
 console.log(a.lt(b));
+*/
+
+function bang() {
+    var clp = new Clip();
+    var notes: Note[] = clp.getSelectedNotes();
+    for (let note of notes) {
+        post(note.toString());
+    }
+}
