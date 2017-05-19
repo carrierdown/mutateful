ca13dd6f246e9ff0d5da840f47da52fc0a80cf72
carrierdown <kar@dips.no>
2017-05-19 15:25:15 +0200

    Add .NET core project

---
+++ src/mutate4l/CliHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mutate4l
+{
+    class CliHandler
+    {
+        public static void Start()
+        {
+            var command = ""; 
+
+            while (command != "q" && command != "quit")
+            {
+                if (command == "h" || command == "help")
+                {
+                    Console.WriteLine("Help text coming here");
+                }
+                Console.WriteLine("Mutate4L: Enter command, or h[elp] / q[uit]");
+                Console.Write("> ");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/mutate4l/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mutate4l
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CliHandler.Start();
+        }
+    }
+}
