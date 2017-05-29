9baef3e86d1d2d81136e9c90a783258348d28c78
Knut Andreas Ruud <kr@upland.no>
2017-05-29 23:07:03 +0200

    Added todo

--- src/mutate4l/ClipProcessor.cs
+++ src/mutate4l/ClipProcessor.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Mutate4l
 {
-    class ClipProcessor
+    public class ClipProcessor
     {
+        public void ProcessCommand(Command command)
+        {
+            // call out to OSC-layer to fetch actual data needed to process the command
+            // find correct Action class to call and pass in options and data
+            // call out to OSC-layer with resulting data
+        }
     }
 }
