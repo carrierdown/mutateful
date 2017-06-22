5636dab6d1bd78e87f51243358a9ba7dbd97b8fc
carrierdown <kar@dips.no>
2017-06-22 15:02:10 +0200

    SortedList complete

---
+++ src/Mutate4lTests/Core/SortedListTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mutate4l.Core;
+using Mutate4l.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mutate4lTests.Core
+{
+    [TestClass]
+    public class SortedListTest
+    {
+        [TestMethod]
+        public void TestSortedList()
+        {
+            var sortedList = new SortedList<Note>(null);
+            sortedList.Add(new Note(64, 1, 1, 100));
+            sortedList.Add(new Note(60, 0, 1, 100));
+            sortedList.Add(new Note(60, .5m, 1, 100));
+            sortedList.Add(new Note(60, .1m, 1, 100));
+            sortedList.Add(new Note(60, 2.00001m, 1, 100));
+            sortedList.Add(new Note(60, 7, 1, 100));
+            sortedList.Add(new Note(60, 12.5m, 1, 100));
+            sortedList.Add(new Note(60, 10, 1, 100));
+            sortedList.Add(new Note(60, 2, 1, 100));
+            sortedList.Add(new Note(60, 1, 1, 100));
+
+            Assert.AreEqual(sortedList[0].Start, 0);
+            Assert.AreEqual(sortedList[1].Start, .1m);
+            Assert.AreEqual(sortedList[2].Start, .5m);
+            Assert.AreEqual(sortedList[3].Start, 1);
+            Assert.AreEqual(sortedList[3].Pitch, 60);
+            Assert.AreEqual(sortedList[4].Start, 1);
+            Assert.AreEqual(sortedList[4].Pitch, 64);
+            Assert.AreEqual(sortedList[5].Start, 2);
+            Assert.AreEqual(sortedList[6].Start, 2.00001m);
+            Assert.AreEqual(sortedList[7].Start, 7);
+            Assert.AreEqual(sortedList[8].Start, 10);
+            Assert.AreEqual(sortedList[9].Start, 12.5m);
+        }
+    }
+}
--- src/mutate4l/Core/SortedList.cs
+++ src/mutate4l/Core/SortedList.cs
@@ -1,10 +1,66 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Mutate4l.Core
 {
-    class SortedList
+    public class SortedList<T>
     {
+        private readonly List<T> _list;
+        private readonly IComparer<T> _comparer;
+
+        public int Count => _list.Count;
+
+        public bool IsReadOnly => ((IList<T>)_list).IsReadOnly;
+
+        public T this[int index] { get => _list[index]; }
+
+        public SortedList(IComparer<T> comparer)
+        {
+            _comparer = comparer ?? Comparer<T>.Default;
+            _list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            var index = _list.BinarySearch(item);
+            _list.Insert(index < 0 ? ~index : index, item);
+        }
+
+        public int IndexOf(T item)
+        {
+            return _list.IndexOf(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            _list.RemoveAt(index);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _list.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return _list.Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IList<T>)_list).GetEnumerator();
+        }
     }
 }
