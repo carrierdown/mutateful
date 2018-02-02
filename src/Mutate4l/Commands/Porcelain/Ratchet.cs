a314f18dfd844001ebe568ce3c3d4f8ab2c4c85b
Knut Andreas Ruud <kr@upland.no>
2018-02-02 10:21:17 +0100

    Started ratchet

---
+++ src/Mutate4l/Commands/Porcelain/Ratchet.cs
@@ -0,0 +1,31 @@
+﻿using Mutate4l.Dto;
+using Mutate4l.Options;
+using System.Collections.Generic;
+
+namespace Mutate4l.Commands.Porcelain
+{
+    public class Ratchet
+    {
+        // Needs control sequence and target sequence
+        // If only one clip is specified, clip will be both control and target sequence
+        // When shaping the ratchet, velocity might be another variable used by the control sequence to control extent of shaping
+        public static ProcessResult Apply(RatchetOptions options, params Clip[] clips)
+        {
+            if (clips.Length < 2)
+            {
+                clips = new Clip[] { clips[0], clips[0] };
+            }
+
+            Clip controlSequence = new Clip(clips[0]);
+            Clip targetSequence = new Clip(clips[1]);
+            Clip resultSequence = new Clip(targetSequence.Length, targetSequence.IsLooping);
+            Dictionary<int, bool> processedIndexes = new Dictionary<int, bool>(targetSequence.Notes.Count);
+
+            // find highest and lowest pitch value in control seq
+
+            // set pitch for each targetNote = (val - minVal) / (maxVal - minVal) * (max - min), corresponding to actual ratchet-value for this event
+
+            return new ProcessResult(clips);
+        }
+    }
+}
---
+++ src/Mutate4l/Options/RatchetOptions.cs
@@ -0,0 +1,15 @@
+﻿using Mutate4l.Core;
+
+namespace Mutate4l.Options
+{
+    public class RatchetOptions
+    {
+        [OptionInfo(min: 1, max: 20)]
+        public int Min { get; set; } = 1;
+
+        [OptionInfo(min: 1, max: 20)]
+        public int Max { get; set; } = 3;
+
+        public decimal[] Shape { get; set; } = new decimal[] { .5m, .5m };
+    }
+}
