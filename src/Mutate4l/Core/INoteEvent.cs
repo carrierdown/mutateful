52b4d7839e33e4a55822e4e336d5f9b9788b0e09
Knut Andreas Ruud <kr@upland.no>
2018-06-04 22:53:02 +0200

    Missing file

---
+++ src/Mutate4l/Core/INoteEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Mutate4l.Core
+{
+    public interface INoteEvent
+    {
+        decimal Start { get; }
+        decimal End { get; }
+        decimal Duration { get; }
+    }
+}
