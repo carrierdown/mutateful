60dc1654af14546d03711bec0a539d3e719ff579
Knut Andreas Ruud <kr@upland.no>
2019-10-15 20:29:50 +0200

    Multiple enums, default values now in place except for enums.

--- src/DocGeneration/Program.cs
+++ src/DocGeneration/Program.cs
@@ -20,15 +20,20 @@ namespace DocGeneration
         
         private static void Main(string[] args)
         {
+            // todo: default values, decimal when not used as MusicalDivision
+            
             var srcFiles = Directory.EnumerateFiles(Path.Join(Environment.CurrentDirectory, @"..\..\..\..\Mutate4l\Commands\"));
             var commandReference = new StringBuilder();
 
             foreach (var srcFile in srcFiles)
             {
-                var (enumName, enumValues) = ExtractEnumData(srcFile);
-                if (enumName.Length > 0)
+                var enums = ExtractEnumData(srcFile);
+                if (enums.Keys.Count > 0)
                 {
-                    ParameterTypes.Add(enumName, string.Join("&#124;", enumValues));
+                    foreach (var key in enums.Keys)
+                    {
+                        ParameterTypes.Add(key, string.Join("&#124;", enums[key]));
+                    }
                 }
             }
             
@@ -43,7 +48,7 @@ namespace DocGeneration
         private static string GetCommandReferenceRow(string[] lines)
         {
             var inOptionsBlock = false;
-            var options = new Dictionary<string, (string, string)>();
+            var options = new Dictionary<string, (string, string, string)>();
             var optionInfo = "";
             var commandName = "";
             var commandDescription = "";
@@ -90,7 +95,34 @@ namespace DocGeneration
                         if (parts.Length < 3) continue;
                         var optionName = parts[2];
                         var optionType = parts[1];
-                        options.Add(optionName, (optionType, optionInfo));
+                        var defaultValue = "";
+                        var equalsIx = Array.IndexOf(parts, "=");
+                        if (equalsIx >= 0)
+                        {
+                            if (parts[equalsIx + 1] == "new")
+                            {
+                                if (parts[equalsIx + 2].Contains("[]"))
+                                {
+                                    defaultValue = parts[equalsIx + 4];
+                                    Console.WriteLine("Setting default to " + defaultValue);
+                                }
+                            }
+                            else if (parts[equalsIx + 1] == "{")
+                            {
+                                defaultValue = parts[equalsIx + 2];
+                                Console.WriteLine("Setting default to " + defaultValue);
+                            }
+                            else
+                            {
+                                defaultValue = parts[equalsIx + 1].Trim(';');
+                            }
+                        }
+
+                        if (defaultValue.Contains('m') && defaultValue.Contains('.'))
+                        {
+                            defaultValue = defaultValue.Substring(0, defaultValue.Length - 1);
+                        }
+                        options.Add(optionName, (optionType, optionInfo, defaultValue));
                         optionInfo = "";
                     }
                 }
@@ -102,7 +134,7 @@ namespace DocGeneration
             foreach (var key in options.Keys)
             {
                 var prepend = false;
-                var (type, info) = options[key];
+                var (type, info, @default) = options[key];
                 if (info.Contains("OptionType.Default"))
                 {
                     prepend = true;
@@ -111,7 +143,7 @@ namespace DocGeneration
                 {
                     formattedOptions.Add(FormatOptionName(key));
                 }
-                var formattedType = FormatTypeDescription(type);
+                var formattedType = FormatTypeDescription(type, @default);
                 if (formattedType.Length > 0)
                 {
                     if (prepend) formattedOptions.Insert(0, formattedType);
@@ -127,8 +159,9 @@ namespace DocGeneration
             return output.ToString();
         }
 
-        private static (string, List<string>) ExtractEnumData(string filename)
+        private static Dictionary<string, List<string>> ExtractEnumData(string filename)
         {
+            var enums = new Dictionary<string, List<string>>();
             var enumName = "";
             var enumValues = new List<string>();
             var lines = File.ReadAllLines(filename);
@@ -140,7 +173,11 @@ namespace DocGeneration
                 {
                     if (line.Trim() == "}")
                     {
-                        break;
+                        enums.Add(enumName, enumValues);
+                        enumValues = new List<string>();
+                        enumName = "";
+                        inEnumBlock = false;
+                        continue;
                     }
 
                     if (!(line.Trim() == "{" || line.Trim() == "}" || line.Trim().StartsWith("/")))
@@ -163,7 +200,7 @@ namespace DocGeneration
                 
             }
 
-            return (enumName, enumValues);
+            return enums;
         }
         
         private static bool IsComment(string line)
@@ -183,13 +220,18 @@ namespace DocGeneration
             return command.ToLower();
         }
 
-        private static string FormatTypeDescription(string type)
+        private static string FormatTypeDescription(string type, string @default)
         {
+            var result = "";
             if (ParameterTypes.ContainsKey(type))
             {
-                return ParameterTypes[type];
+                result = ParameterTypes[type];
+                if (@default.Length > 0 && @default != "true" && @default != "false")
+                {
+                    result = result.Trim('>') + ": " + @default + ">";
+                }
             }
-            return "";
+            return result;
         }
     }
 }
