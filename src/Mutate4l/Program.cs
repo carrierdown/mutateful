303b32b13508de3fb9c66f93a72692be7e1758b0
Knut Andreas Ruud <kr@upland.no>
2020-01-05 19:53:57 +0100

    Working async udp send and receive using separate tasks running in parallel.

--- src/Mutate4l/Program.cs
+++ src/Mutate4l/Program.cs
@@ -70,23 +70,26 @@ namespace Mutate4l
             {
                 // this should trigger some event that notifies the state of mutateful and possibly triggers a re-evaluation of any formulas (should take into account whether the received data is for a complete clip or just partial)
                 Console.WriteLine($"Received datagram of size {values.Length}");
-                await writer.WriteAsync(new byte[]
-                {
-                    104, 2, 0, 0, 128, 64, 1, 13, 0, 60, 0, 0, 0, 0, 0, 0, 128, 62, 100, 62, 0, 0, 128, 62, 0, 0, 128, 62, 100,
-                    63, 0, 0, 0, 63, 0, 0, 128, 62, 100, 65, 0, 0, 64, 63, 0, 0, 128, 62, 100, 66, 0, 0, 128, 63, 0, 0, 128, 62,
-                    100, 67, 0, 0, 160, 63, 0, 0, 128, 62, 100, 70, 0, 0, 192, 63, 0, 0, 128, 62, 100, 72, 0, 0, 224, 63, 0, 0,
-                    128, 62, 100, 60, 0, 0, 48, 64, 0, 0, 128, 62, 100, 62, 0, 0, 64, 64, 0, 0, 128, 62, 100, 63, 0, 0, 80, 64, 0,
-                    0, 128, 62, 100, 64, 0, 0, 96, 64, 0, 0, 128, 62, 100, 67, 0, 0, 112, 64, 0, 0, 128, 62, 100
-                });
+                var result = CliHandler.HandleData(values);
+                if (result.Length > 0) 
+                    await writer.WriteAsync(result);
             }
         }
         
         public static async Task ConsumeQueue(UdpClient udpClient, ChannelReader<byte[]> reader)
         {
-            await foreach (var item in reader.ReadAllAsync()/*.ConfigureAwait(false)*/)
+            await foreach (var clipData in reader.ReadAllAsync().ConfigureAwait(false))
             {
-                Console.WriteLine($"Received data to send, with length {item.Length}");
-                udpClient.Send(item, item.Length, EndPoint);
+                Console.WriteLine($"Received data to send, with length {clipData.Length}");
+                try
+                {
+                    udpClient.Send(clipData, clipData.Length, "127.0.0.1", 8023);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Exception occurred while sending UDP data");
+                }
+                await Task.Delay(10);
             }
         }
     }
