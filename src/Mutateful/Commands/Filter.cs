c91d5277329fb474edd9b81c60396bffbda14960
Knut Andreas Ruud <kr@upland.no>
2022-01-08 08:12:29 +0100

    Filter command: Fix wrong default value for duration

--- src/Mutateful/Commands/Filter.cs
+++ src/Mutateful/Commands/Filter.cs
@@ -8,7 +8,7 @@ namespace Mutateful.Commands
     public class FilterOptions
     {
         [OptionInfo(OptionType.Default, 1/512f)]
-        public decimal Duration { get; set; } = 1/64m;
+        public decimal Duration { get; set; } = 4/64m;
 
         public bool Invert { get; set; }
     }
